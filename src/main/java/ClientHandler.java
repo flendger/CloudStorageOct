316b393161b5e3edf20aa65e16e8ba02f7c2984c
flendger <kiru.ee@yandex.ru>
2020-10-29 19:42:52 +0300

    Реализовано getFile для клиента Реализована команда download для сервера

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,8 +1,4 @@
-import javax.security.sasl.SaslException;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
 
@@ -41,7 +37,25 @@ public class ClientHandler implements Runnable {
                     }
                 }
                 if (command.equals("download")) {
-                    // TODO: 27.10.2020
+                    File file = new File("server/" + in.readUTF());
+                    if (file.exists()) {
+                        out.writeUTF("EXISTS");
+
+                        long length = file.length();
+                        out.writeLong(length);
+
+                        FileInputStream fileBytes = new FileInputStream(file);
+                        int read;
+                        byte[] buffer = new byte[256];
+                        while ((read = fileBytes.read(buffer)) != -1) {
+                            out.write(buffer, 0, read);
+                        }
+                        out.flush();
+                        String status = in.readUTF();
+                        System.out.println(status);
+                    } else {
+                        out.writeUTF("NOT EXIST");
+                    }
                 }
                 if (command.equals("exit")) {
                     System.out.println("Client disconnected correctly");
