c3c4a8ca3ae5114558da95054cccebeab79c8403
flendger <kiru.ee@yandex.ru>
2020-10-29 19:42:52 +0300

    Реализовано getFile для клиента Реализована команда download для сервера

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -48,7 +48,34 @@ public class Client extends JFrame {
     }
 
     private void getFile(String fileName) {
-        // TODO: 27.10.2020
+        try {
+            out.writeUTF("download");
+            out.writeUTF(fileName);
+
+            String fileExists = in.readUTF();
+            System.out.println(fileExists);
+            if (fileExists.equals("EXISTS")) {
+                try {
+                    File file = new File("client/" + fileName);
+                    if (!file.exists()) {
+                        file.createNewFile();
+                    }
+                    long size = in.readLong();
+                    FileOutputStream fos = new FileOutputStream(file);
+                    byte[] buffer = new byte[256];
+                    for (int i = 0; i < (size + 255) / 256; i++) {
+                        int read = in.read(buffer);
+                        fos.write(buffer, 0, read);
+                    }
+                    fos.close();
+                    out.writeUTF("OK");
+                } catch (Exception e) {
+                    out.writeUTF("WRONG");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void sendFile(String filename) {
@@ -59,7 +86,7 @@ public class Client extends JFrame {
             long length = file.length();
             out.writeLong(length);
             FileInputStream fileBytes = new FileInputStream(file);
-            int read = 0;
+            int read;
             byte[] buffer = new byte[256];
             while ((read = fileBytes.read(buffer)) != -1) {
                 out.write(buffer, 0, read);
